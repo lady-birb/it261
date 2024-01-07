c278b16707b4682139ba4b4a7b7f191c51116e52
Tori <149717665+lady-birb@users.noreply.github.com>
2024-01-06 17:06:57 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible"
+    content="ie=edge">
+    <title>My Portal Page</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<h1><a href="index.php">Tori's Portal Page</a></h1>    
+<h2>The navigation below represents our BIG Assignments.</h2>
+<div id="wrapper">
+<nav>
+<ul>
+
+<li><a href="">Switch</a></li>
+<li><a href="">Troubleshoot</a></li>
+<li><a href="">Calculator</a></li>
+<li><a href="">Email</a></li>
+<li><a href="">Database</a></li>
+<li><a href="">Gallery</a></li>
+</ul>
+
+</nav>
+
+<main>
+<h2>About Tori</h2>
+<img class="right" src="../it261/website/images/logo.png" alt="Tori">
+<p>A paragraph abput yourself, i.e., goals, background, hobbies etc! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem amet neque est autem magni possimus libero, et suscipit maiores quia explicabo quos, sunt perferendis. Possimus veniam commodi facere? Quia, suscipit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem beatae accusamus amet maiores obcaecati. Asperiores nam expedita earum perferendis ab aliquam minus, doloribus autem odio architecto saepe, magnam eveniet distinctio?</p>
+
+</main>
+
+
+<aside>
+<h2>Weekly Class Exercises</h2>
+<h3>Week 2</h3>
+<ul>
+<ol>
+<li><a href="var.php">var.php</a></li>
+<li><a href="var2.php">var2.php</a></li>
+<li><a href="var.php">something.php</a></li>
+<li><a href="var.php">something.php</a></li>
+</ol>
+
+
+</ul>
+
+
+</aside>
+
+</div>
+<!-- end wrapper -->
+
+
+</body>
+</html>
