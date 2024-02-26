4f7ed91f4f1e8a7ba9ea3a509ef745b4bc2652c9
Tori <149717665+lady-birb@users.noreply.github.com>
2024-02-25 16:02:41 -0800

    Add files via upload

---
+++ website/gallery.php
@@ -0,0 +1,16 @@
+
+<?php
+include('config.php');
+include('./includes/header.php');
+?>
+<div id="wrapper">
+<main>
+ <h1>Welcome to my Sanrio gallery</h1> 
+ <p>Please enjoy this gallery of some of my favorite Sanrio character!</p>
+<?php
+include('./includes/galleryphp.php');
+?>
+</main>
+</div>
+<?php
+include('./includes/footer.php'); 
